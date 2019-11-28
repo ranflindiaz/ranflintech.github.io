841256a95ab221c18d1af4f5cc5e1de349b72c7b
ranflintech <ranflinpc@users.noreply.github.com>
2019-11-27 21:51:58 -0500

    Adding statir project

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('myBtn');
+let texto = document.getElementById('myText');
+
+const changeColor = () => {
+    document.getElementById('caja').style.background = texto.value;
+}
+
+button.addEventListener('click', changeColor);
+
+texto.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        changeColor();
+    }
+});
+
+
